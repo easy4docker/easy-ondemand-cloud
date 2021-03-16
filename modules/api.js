1772e5a33f035e2cc28d660e532b30cb20c76ece
easy4docker <easy4docker@gmail.com>
2021-03-16 14:17:28 -0700

    update

--- modules/api.js
+++ modules/api.js
@@ -1,7 +1,8 @@
 (function () { 
 	var obj =  function (env, pkg) {
-        const fs = require('fs'),
-            me = this;
+        const 	me = this,
+				fs = require('fs'),
+				cp = new pkg.crowdProcess()
 
 		me.call = (postData, callback) => {
 			switch(postData.cmd) {
@@ -31,12 +32,27 @@
 		me.getResultFiles = (postData, callback) => {
 			if (!postData || !postData.data || !postData.data.result) {
 				callback({status:'failure', message : 'Missing postData.data.result'});
-			} else {
-				fs.readdir(env.sharedFolder + '/' + postData.data.result, (err, list) => {
-					callback({status:'success', result: (err) ? [] : list.filter(
-						(rec) => { return (rec[0] === '.') ? false: true})});
+				return true;
+			}
+			const _f = {};
+			_f['input'] = (cbk) => {
+				fs.readdir(env.sharedFolder + '/' + postData.data.result +  '/input', (err, list) => {
+					cbk((err) ? [] : list.filter(
+						(rec) => { return (rec[0] === '.') ? false: true}));
 				});
 			}
+			_f['output'] = (cbk) => {
+				fs.readdir(env.sharedFolder + '/' + postData.data.result + '/output', (err, list) => {
+					cbk((err) ? [] : list.filter(
+						(rec) => { return (rec[0] === '.') ? false: true}));
+				});
+			}
+			cp.serial(_f, (result) => {
+				callback({status:'success', files :{
+					input  : cp.data.input,
+					output : cp.data.output
+				}});
+			}, 3000)
 		}
 	};
 	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
--- www/vueApp/easyOndemandDemo/body/ondemandResults.vue
+++ www/vueApp/easyOndemandDemo/body/ondemandResults.vue
@@ -1,28 +1,24 @@
 <template>
-    <div class="body-card m-1">
+    <div class="body-card overflow-auto  m-1">
         <div class="card-body m-0 p-1">
             <div class="container-fluid m-0 head-menu-1">
                 <div class="row">
-                    <div class="col-3 p-1 m-0 ">
+                    <div class="col-2 p-1 m-0 ">
                         <div class="card ondemand-requestions-section mt-0 mr-1 p-2">
                             <div class="pl-2 m-0 text-left"><h5>OnDemand Results:</h5></div>
 
                             <div v-if="currentResult" class="border border-secondary alert-secondary rounded m-1 p-1 text-left">
                                 <div class="p-1">
                                     {{currentResult}}
-                                    ({{resultFiles.length}})
+                                    ({{/*resultFiles.length*/}})
                                     <a href="JavaScript:void(o)" v-on:Click="togglesRresultFiles()"  >
                                         <i class="fa fa-angle-double-up pull-right" v-if="!togglesRresult"></i>
                                         <i class="fa fa-angle-double-right pull-right" v-if="!!togglesRresult"></i>
                                     </a>
                                 </div>
                                 <div class="current-ondemand-section overflow-auto bg-secondary" v-if="!!togglesRresult">
-                                    <div v-for="o in resultFiles" class="ml-2 mt-1">
-                                        <a href="JavaScript:void(0)"
-                                            class="text-left text-light" >
-                                            {{o}}
-                                        </a>
-                                    </div>
+                                    Input :  {{(!resultFiles.input) ? '' : resultFiles.input.length}}<br/>
+                                    Output : {{(!resultFiles.input) ? '' : resultFiles.output.length}}<br/>
                                 </div>  
                             </div>
                             <span v-if="results.length" v-for="o in results">
@@ -38,23 +34,23 @@
                         </div>
                     
                     </div>
-                    <div class="card alert-light col-9 p-2 m-0 text-left">
+                    <div class="card alert-light col-10 p-2 m-0 text-left">
                         <div v-if="!results.length"
                             class="m-1 p-1 text-left text-secondary">
                             No results.
-                        </div> 
-                        <div class="" v-if="!!togglesRresult">
-                            Input Data:
-                        </div>  
-                        <div class="" v-if="!!togglesRresult">
-                            Output Data:
-                            <div class="alert-secondary">
-                                <div v-for="o in resultFiles" class="ml-2 mt-1">
-                                    <a href="JavaScript:void(0)"
-                                        class="text-left text-light" >
-                                        {{o}}
-                                    </a>
-                                </div>
+                        </div>
+                        Input Data: 
+                        <div class="input-data-section alert-warning rounded border border-warning p-1 overflow-auto">
+                            input contents
+                        </div>
+                        Output Data:
+                        <div class="output-data-section alert-success rounded border border-success p-1 overflow-auto">
+                            output contents
+                            <div v-for="o in resultFiles" class="ml-2 mt-1">
+                                <a href="JavaScript:void(0)"
+                                    class="text-left text-secondary" >
+                                    {{o}}
+                                </a>
                             </div>
                         </div>  
                     </div>
@@ -105,7 +101,7 @@ module.exports = {
                 data : { result : o }
             }, (result)=> {
                 me.togglesRresult = true;
-                me.resultFiles = result.result;
+                me.resultFiles = result.files;
                 console.log(result);
             }, true);
         },
@@ -124,5 +120,12 @@ module.exports = {
 }
 .current-ondemand-section {
     max-height: 26rem;
+}
+.input-data-section {
+    height: 12rem;
+}
+.output-data-section {
+    min-height: 18rem;
+    max-height: 36rem;
 }
 </style>
