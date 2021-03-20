859e175876891c7b1024baae0cd95a17b6927955
easy4docker <easy4docker@gmail.com>
2021-03-20 10:04:56 -0700

    Update moduleUpload.js

--- modules/moduleUpload.js
+++ modules/moduleUpload.js
@@ -6,13 +6,38 @@ const { cpuUsage } = require('process');
             fs = require('fs'),
             exec = require('child_process').exec,
             CP = new pkg.crowdProcess();
-
+        /*
         me.afterUpload = () => {
             const uploadID = (req.body.uploadID) ? req.body.uploadID : new Date().getTime();
-            /*
-            {"status":"success","uploadID":1616253409355,"files":[{"fieldname":"data[selectedFile]","originalname":"DocQaletDocker2","encoding":"7bit","mimetype":"application/octet-stream","destination":"/tmp/uploaded","filename":"7129b97fd5d5a62b00128c62174a91f8","path":"/tmp/uploaded/7129b97fd5d5a62b00128c62174a91f8","size":7578},{"fieldname":"fileBB","originalname":"readme.txt","encoding":"7bit","mimetype":"plain/text","destination":"/tmp/uploaded","filename":"d93243ee1371bcf319fe3fe9af05c04d","path":"/tmp/uploaded/d93243ee1371bcf319fe3fe9af05c04d","size":11}]}
-            */
-            res.send({status: 'success1', uploadID: uploadID, files : (!req.files) ? [] : req.files});
+            {"status":"success1","uploadID":1616258000940,"files":[{"fieldname":"data[selectedFile]","originalname":"DocQaletDocker2","encoding":"7bit","mimetype":"application/octet-stream","destination":"/tmp/uploaded","filename":"e4211d57ad6baaad5455ec510e688a02","path":"/tmp/uploaded/e4211d57ad6baaad5455ec510e688a02","size":7578},{"fieldname":"fileBB","originalname":"readme.txt","encoding":"7bit","mimetype":"plain/text","destination":"/tmp/uploaded","filename":"9016c36d9b0b268e6b49ad161d6cd079","path":"/tmp/uploaded/9016c36d9b0b268e6b49ad161d6cd079","size":11}]}
+            let files = (!req.files) ? [] : req.files, 
+                cmd = '';
+            for (var i = 0; i < files; i++) {
+                cmd += ((cmd) ? ' && ' : '') + ' mv ' + files[i].path + ' ' + files[i].destination + '/F_' + uploadID + '_' + files[i].originalname
+            }
+            exec(cmd, {maxBuffer: 224 * 2048}, (err, stdout, stderr) => {
+                res.send({status: 'success1', uploadID: uploadID, files : (!req.files) ? [] : req.files});
+            })
+        }
+*/
+        me.afterUpload = () => {
+            const uploadID = (req.body.uploadID) ? req.body.uploadID : new Date().getTime();
+            let files = (!req.files) ? [] : req.files, 
+                flist = [],
+                cmd = '';
+            for (var i = 0; i < files.length; i++) {
+                const fname = files[i].destination + '/F_' + uploadID + '_' + files[i].originalname;
+                flist.push(fname);
+                cmd += ((cmd) ? ' && ' : '') + 'mv ' + files[i].path + ' ' + fname;
+            }
+            if (cmd) {
+                exec(cmd, {maxBuffer: 224 * 2048}, (err, stdout, stderr) => {
+                    res.send({status: 'success', uploadID: uploadID, files : flist});
+                })
+            } else {
+                res.send({status: 'failure', message : 'nothing uploaded!'});
+            }
+
         }
     }
     module.exports = obj;
